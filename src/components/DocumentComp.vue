a97b41d88cd93ce8ce931903f5ead8712860ab1b
Aekhy <102717242+Aekhy@users.noreply.github.com>
2023-12-07 20:48:22 +0100

    wip : question dummy

--- src/components/DocumentComp.vue
+++ src/components/DocumentComp.vue
@@ -1,8 +1,29 @@
 <script setup>
-
+import { ref } from "vue";
 // une question reçu par la page
 
-// des réponses 
+
+// defineProps({
+//   question: String,
+//   
+// })
+
+const question = ref({
+    id_question: 1,
+    question : "Quel est la bonne réponse ? (A)",
+    explication : "A est toujours la meilleur réponse",
+    reponses : [{
+        id_reponse : 1,
+        reponse : "A",
+        exemplevar1 : -2
+    }]
+});
+
+// function choisirReponse(id_reponse){
+//     $emit('CHANGEME', question)
+// }
+
+
 
 </script>
 <template>
